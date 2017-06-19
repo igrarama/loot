c7e12c8cd24be8548fe971385d42bbab9ff6d89c
Ron Yoskovich <ryoskovich@gmail.com>
2017-06-19 15:34:01 +0300

    added item card component

---
+++ client/app/components/itemCard/itemCard.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './itemCard.scss';
+
+class ItemCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+ItemCard.propTypes = {
+
+};
+
+export default ItemCard;
