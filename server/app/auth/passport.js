28d9e0b8c91343698bed40c14493b5c8c2418a64
Dmitry Dodzin <d.dodzin@gmail.com>
2017-06-19 20:54:31 +0300

    Adding fetch from DB for auth

--- server/app/auth/passport.js
+++ server/app/auth/passport.js
@@ -1,18 +1,28 @@
 
+const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = (passport) => {
+
   passport.serializeUser((user, done) => done(null, user.id));
 
   passport.deserializeUser((id, done) => {
-    done(null, { id });
+    mongoose.model('Person').findOne({ id })
+      .then(user => done(null, user))
+      .catch(err => done(err));
   });
 
   passport.use(new LocalStrategy(
     function(username, password, done) {
-      if(username == 't' && password == 't')
-        return done(null, { id: 1 });
-      return done(null, false);
+
+      //TODO: Addd check for user password, currently this
+
+      if(username == 't' && password == 't'){
+        mongoose.model('Person').findById('59480f2ca7450598758b1eae')
+          .then(user => done(null, user));
+      } else {
+        done(null, false);
+      }
     }
   ));
 };
