f0f7f2a63057f763ac0ac082ee1ee4d0f1f63864
Dmitry Dodzin <d.dodzin@gmail.com>
2017-06-19 20:42:46 +0300

    Adding configs and moving the passport to app

--- server/app/data/models.js
+++ server/app/data/models.js
@@ -1,10 +1,4 @@
-const mongoose = require('mongoose');
 const fs = require('fs');
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-// });
 
 function initSchemas() {
     const files = fs.readdirSync(__dirname + '/schemas');
