c3e14b1d5c45a1b98ff1cf50a81f7b29005a46d5
mickey007 <mikaw2@hotmail.com>
2017-06-20 03:00:54 +0300

    added person card with css

--- client/app/components/requestCard/requestCard.component.jsx
+++ client/app/components/requestCard/requestCard.component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import './requestCard.scss';
+import PersonCard from '../personCard/personCard.component';
 
 class RequestCard extends Component {
     constructor() {
@@ -16,33 +17,16 @@ class RequestCard extends Component {
         this.setState({ isExpanded: !isExpanded });
     }
 
-    displayPerson = (customer) => {
-        return <div className="person">
-            <div className="name">
-                <span>{ customer.firstName }</span>
-                <span>{ customer.lastName }</span>
-            </div>
-            <div className="info">
-                <span>{ customer.personalNumber }</span>
-                <span>{ customer.releaseDate }</span>
-            </div>
-            <div className="contact-info">
-                <span>{ customer.email }</span>
-                <span>{ customer.voip }</span>
-                <span>{ customer.phoneNumber }</span>
-            </div>
-        </div>;
-    }
-
     displayOrder = (products) => {
-        products = _.groupBy(products, 'id');
-        products = Object.keys(products).map(key => {
+        let groupedProducts = _.groupBy(products, 'name');
+        products = Object.keys(groupedProducts).map(key => {
             return {
-                name: products[key][0].name,
-                isInUse: products[key][0].isInUse,
-                count: products[key].length
+                name: groupedProducts[key][0].name,
+                isInUse: groupedProducts[key][0].isInUse,
+                count: groupedProducts[key].length
             };
         });
+        console.log(products);
         return <div className="order">
             <span>products</span>
             <ul>
@@ -60,8 +44,8 @@ class RequestCard extends Component {
     render() {
         let { customer, products, reason, comments } = this.props.item;
         return (
-            <div className={ 'item1-card ' + this.props.typeColor}>
-                { this.displayPerson(customer) }
+            <div className={ 'item1-card green'}>
+                <PersonCard customer={customer}/>
                 { this.displayOrder(products) }
                 <div className="reason">
                     <span> : { reason }</span>
@@ -74,8 +58,7 @@ class RequestCard extends Component {
 }
 
 RequestCard.propTypes = {
-    item: PropTypes.object.isRequired,
-    typeColor: PropTypes.string
+    item: PropTypes.object.isRequired
 };
 
 RequestCard.defaultProps = {
